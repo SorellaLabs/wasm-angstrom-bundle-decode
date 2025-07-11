e626c58ea29420d8699b8b9407e6d97e6c0e3e8c
jnoorchashm37 <joseph@sorellalabs.xyz>
2025-07-11 11:45:45 -0400

    no comments

--- build-test.sh
+++ build-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# if directory `rs-bundle-decode` doesn't exist, exit
 if [ ! -d "rs-bundle-decode" ]; then
     echo "Error: Directory 'rs-bundle-decode' does not exist"
     exit 1
@@ -10,13 +9,11 @@ cp -r rs-bundle-decode/pkg ts-bundle-decode/pkg
 rm ts-bundle-decode/pkg/.gitignore
 
 wasm-pack build --target web rs-bundle-decode
-# if the above command fails, exit
 if [ $? -ne 0 ]; then
     echo "Error: wasm-pack build failed"
     exit 1
 fi
 
-# if directory `ts-bundle-decode` doesn't exist, exit
 if [ ! -d "ts-bundle-decode" ]; then
     echo "Error: Directory 'ts-bundle-decode' does not exist"
     exit 1
@@ -24,7 +21,7 @@ fi
 
 npm i --prefix ts-bundle-decode
 npm run build --prefix ts-bundle-decode
-# if the above command fails, exit
+
 if [ $? -ne 0 ]; then
     echo "Error: npm start failed"
     exit 1
